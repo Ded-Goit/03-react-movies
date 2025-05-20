63aa72296c3127dd52875eeb773932ae2ab65088
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-20 13:15:36 +0300

    added MovieGrid DOM element

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
 
 export default function App() {
   return (
     <div className={styles.app}>
       <SearchBar />
+      <MovieGrid />
     </div>
   );
 }
