9e81ea7f5ce556454a2ce93f1854ae806836dcbb
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-20 13:24:12 +0300

    added Loader end ErrorMessage DOM element

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,16 @@
 import styles from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export default function App() {
   return (
     <div className={styles.app}>
       <SearchBar />
       <MovieGrid />
+      <Loader />
+      <ErrorMessage />
     </div>
   );
 }
