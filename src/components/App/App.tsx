8ed8db42b56f69d2d988d29acac21c972b8cb148
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-20 17:02:18 +0300

    added MovieModal component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,15 +4,24 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
+import type { Movie } from "../../types/movie";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
   return (
     <div className={styles.app}>
       <SearchBar />
       <MovieGrid />
       <Loader />
       <ErrorMessage />
-      <MovieModal />
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
     </div>
   );
 }
--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,14 +1,67 @@
 import css from "./MovieModal.module.css";
+import type { Movie } from "../../types/movie";
+import { useEffect } from "react";
 
-export default function MovieModal() {
+interface MovieModalProps {
+  movie: Movie;
+  onClose: () => void;
+}
+
+export default function MovieModal({ movie, onClose }: MovieModalProps) {
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  /*useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") onClose();
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if ((event.target as HTMLElement).classList.contains(css.backdrop)) {
+        onClose();
+      }
+    };
+    document.body.style.overflow = "hidden";
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      document.body.style.overflow = "";
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [onClose]);*/
+
   return (
-    <div className={css.backdrop} role="dialog" aria-modal="true">
+    <div
+      className={css.backdrop}
+      role="dialog"
+      onClick={handleBackdropClick}
+      aria-modal="true"
+    >
       <div className={css.modal}>
         <button className={css.closeButton} aria-label="Close modal">
           &times;
         </button>
         <img
-          src="https://image.tmdb.org/t/p/original/backdrop_path"
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
           alt="movie_title"
           className={css.image}
         />
