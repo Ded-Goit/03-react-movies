62436a252081f8ef7ce6286e7ca2785e89f73a88
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-21 20:21:30 +0300

    added movieService and token

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,18 @@
+//import axios from "axios";
 import styles from "./App.module.css";
-import SearchBar from "../SearchBar/SearchBar";
+//import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 import type { Movie } from "../../types/movie";
 import { useState } from "react";
-
+//const myKey = import.meta.env.VITE_TMDB_TOKEN;
 export default function App() {
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const handleSearh = (newQuery: string) => {
-    console.log("handleSearh", newQuery);
-  };
   return (
     <div className={styles.app}>
-      <SearchBar onSearch={handleSearh} />
       <MovieGrid />
       <Loader />
       <ErrorMessage />
